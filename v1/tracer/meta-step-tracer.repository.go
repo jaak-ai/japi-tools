38f3856cfc37e181f2928a6b00104b73fbc1a481
Cristian Ramirez <50112991+rcristian3@users.noreply.github.com>
2022-11-14 14:03:38 -0600

    feat: add meta to step tracer

--- v1/tracer/meta-step-tracer.repository.go
+++ v1/tracer/meta-step-tracer.repository.go
@@ -1,9 +1,14 @@
 package tracer
 
 type MetaStepTracer struct {
-	ProcessTime float32 `bson:"process_time" json:"processTime"`
+	ProcessTime float32     `bson:"process_time" json:"processTime"`
+	Extra       interface{} `bson:"extra" json:"extra"`
 }
 
 func (model *MetaStepTracer) SetProcessTime(value float32) {
 	model.ProcessTime = value
+}
+
+func (model *MetaStepTracer) SetExtra(value interface{}) {
+	model.Extra = value
 }
